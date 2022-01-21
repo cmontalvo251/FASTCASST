d3903191fef3b1343307ad6270f0463921f0037b
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-01-20 20:54:39 -0600

    Added portalcube controller

--- hardware/hardware.h
+++ hardware/hardware.h
@@ -10,7 +10,7 @@
 // 3 - UART Control Matrix (MATLAB) - HIL
 // 4 - UART Sense Matrix (MATLAB) - HIL
 
-//////////Outputs to Hardware Class//////////////
+//////////Outputs to From Hardware Class//////////////
 // 1 - Telemetry Matrix to Ground Station (MATLAB)
 // 2 - RX Matrix to System Controller (MATLAB)
 // 3 - Sense Matrix to System Controller (MATLAB)
@@ -21,14 +21,9 @@
 
 class hardware {
  private:
-  MATLAB hw_control_matrix;
  public:
   //Initialization routine needs the root folder name
   void init(char root_folder_name);
-  //Initialize Control Matrix
-  void initControlMatrix(MATLAB control_matrix);
-  //Also need to receive control matrix
-  void getControlMatrix(MATLAB control_matrix);
   //Constructor
   hardware();
 }

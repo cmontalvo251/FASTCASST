e27eea014734bb8100bd490f1629b79320ef1653
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-02-22 23:24:38 -0600

    Added quadcopter dynamics

---
+++ vehicles/quadcopter/src/controller.cpp
@@ -0,0 +1,138 @@
+//This is a portal cube template. You must adhere to these standards if you write your
+//own
+
+#include "controller.h"
+
+controller::controller() {
+};
+
+void controller::init(MATLAB in_configuration_matrix) {
+  control_matrix.zeros(NUMSIGNALS,1,"Control Signals"); //The standards must be TAERA1A2A3A4
+  set_defaults();
+  printf("Controller Received Configuration Matrix \n");
+  //in_configuration_matrix.disp();
+  CONTROLLER_FLAG = in_configuration_matrix.get(7,1);
+  printf("Controller Setup \n");
+}
+
+void controller::set_defaults() {
+  control_matrix.set(1,1,OUTMID);
+  control_matrix.set(2,1,OUTMID);
+  control_matrix.set(3,1,OUTMID);
+  control_matrix.set(4,1,OUTMID);
+}
+
+void controller::print() {
+  for (int i = 1;i<=NUMSIGNALS;i++) {
+    printf("%d ",int(control_matrix.get(i,1)));
+  }
+}
+
+void controller::loop(double currentTime,int rx_array[],MATLAB sense_matrix) {
+   //The sensor matrix is a 29x1. See sensors.cpp for list of sensors
+  //At a minimum you need to just feed through the rxcomms into the ctlcomms
+  //Which means you can't have more control signals than receiver signals
+
+  //Default Control Signals
+  set_defaults();
+
+  //I want to keep track of timeElapsed so that I can run integrators
+  //and compute derivates
+  elapsedTime = currentTime - lastTime;
+  lastTime = currentTime;
+
+  //At a minimum you need to compute the 4 motor signals based on 
+  //the standard acro mode
+  double motor_upper_left = STICK_MIN;
+  double motor_upper_right = STICK_MIN;
+  double motor_lower_left = STICK_MIN;
+  double motor_lower_right = STICK_MIN;		
+  
+  //First extract the relavent commands from the receiver.
+  double throttle = rx_array[0];
+  double aileron = rx_array[1];
+  double elevator = rx_array[2];
+  double rudder = rx_array[3];
+  double autopilot = rx_array[4];
+  bool icontrol = 0;
+
+  switch (CONTROLLER_FLAG) {
+  case -1:
+    //User decides
+    if (autopilot > STICK_MID) {
+      icontrol = 1;
+    } else {
+      icontrol = 0;
+    }
+    break;
+  case 0:
+    //Always off
+    icontrol = 0;
+    break;
+  case 1:
+    //Always on
+    icontrol = 1;
+    break;
+  }
+
+  //Then you can run any control loop you want.
+  if (icontrol) {
+    //STABILIZE MODE
+    //printf(" STAB ");
+    double roll_command = (aileron-STICK_MID)*50.0/((STICK_MAX-STICK_MIN)/2.0);
+    double pitch_command = -(elevator-STICK_MID)*50.0/((STICK_MAX-STICK_MIN)/2.0);
+    double yaw_rate_command = (rudder-STICK_MID)*50.0/((STICK_MAX-STICK_MIN)/2.0);
+    double roll = sense_matrix.get(4,1);
+    double pitch = sense_matrix.get(5,1);
+    double yaw = sense_matrix.get(6,1);
+    double roll_rate = sense_matrix.get(10,1); //For SIL/SIMONLY see Sensors.cpp
+    double pitch_rate = sense_matrix.get(11,1); //These are already in deg/s
+    double yaw_rate = sense_matrix.get(12,1); //Check IMU.cpp to see for HIL
+    //printf("PQR Rate in Controller %lf %lf %lf \n",roll_rate,pitch_rate,yaw_rate);
+    double kp = 10.0;
+    double kd = 2.0;
+    double kyaw = 5.0;
+    double droll = kp*(roll-roll_command) + kd*(roll_rate);
+    droll = CONSTRAIN(droll,-500,500);
+    double dpitch = kp*(pitch-pitch_command) + kd*(pitch_rate);
+    dpitch = CONSTRAIN(dpitch,-500,500);
+    double dyaw = kyaw*(yaw_rate-yaw_rate_command);
+    //printf("YAW RATE = %lf YAW RATE COMMAND = %lf DYAW = %lf \n",yaw_rate,yaw_rate_command,dyaw);
+    dyaw = CONSTRAIN(dyaw,-500,500);
+
+    //Throttle controller on climb rate
+    /*double climb_rate_command = (throttle-STICK_MID)*10/((STICK_MAX-STICK_MIN/2.0));
+    //printf("Climb Rate Command = %lf \n",climb_rate_command);
+    double climb_rate = sense_matrix.get(9,1);
+    double kpt = 100.0;
+    double dthrottle = -kpt*(climb_rate_command - climb_rate);*/
+    //printf("dthrottle = %lf zdot = %lf \n",dthrottle,climb_rate);
+    
+    double altitude_command = -100.0;
+    double z = sense_matrix.get(3,1);
+    double zdot = sense_matrix.get(9,1);
+    double kpt = 10.0;
+    double kdt = 50.0;
+    double dthrottle = kpt*(z-altitude_command) + kdt*(zdot);
+    
+    //printf("d = %lf %lf %lf ",droll,dpitch,dyaw);
+    //printf(" Roll Command = %lf ",roll_command);
+    motor_upper_left = throttle + dthrottle - droll - dpitch - dyaw;
+    motor_upper_right = throttle + dthrottle + droll - dpitch + dyaw;
+    motor_lower_left = throttle + dthrottle - droll + dpitch + dyaw;
+    motor_lower_right = throttle + dthrottle + droll + dpitch - dyaw;
+  } else {
+    //ACRO MODE
+    motor_upper_left = throttle + (aileron-STICK_MID) - (elevator-STICK_MID) + (rudder-STICK_MID);
+    motor_upper_right = throttle - (aileron-STICK_MID) - (elevator-STICK_MID) - (rudder-STICK_MID);
+    motor_lower_left = throttle + (aileron-STICK_MID) + (elevator-STICK_MID) - (rudder-STICK_MID);
+    motor_lower_right = throttle - (aileron-STICK_MID) + (elevator-STICK_MID) + (rudder-STICK_MID);
+  }
+  
+  //Send the motor commands to the control_matrix values
+  control_matrix.set(1,1,motor_upper_left);
+  control_matrix.set(2,1,motor_upper_right);
+  control_matrix.set(3,1,motor_lower_left);
+  control_matrix.set(4,1,motor_lower_right);
+  //control_matrix.disp();
+}
---
+++ vehicles/quadcopter/src/forces.cpp
@@ -0,0 +1,115 @@
+/* Forces Template 2021
+
+This forces file is a template for a fictitious portalcube
+with thrusters and a simple aero model. The Dynamics.cpp module
+will call a few candidate functions. If you make your own aero
+file with header and cpp file you must conform to the following
+functions otherwise the software will completely break.
+
+*/
+
+#include "forces.h"
+
+//Constructor
+forces::forces() {
+  //The constructor must create these 3x1 vectors
+  FB.zeros(3,1,"Force in Body Frame");
+  MB.zeros(3,1,"Moment in Body Frame");
+  thrust_motors.zeros(4,1,"Force Motors");
+  torque_motors.zeros(4,1,"Torque Motors");
+  
+  //Quadcopter Aerodynamic Parameters
+  double thrust_max = 0.4*GEARTH; //kilograms * gravity
+  //Rotor Size
+  Rrotor = (9.5/12.0)/(2*3.28); //9.5 inch props to meters
+  //Area
+  AREA = PI*pow(Rrotor,2.0);  //m^2
+  //Battery Size
+  double Voltage = 3.7*4; //4 Cell
+  //KV Rating
+  double KV = 950.0;
+  //angular velocity is
+  double omegaRPM = KV*Voltage;
+  double omegarads = omegaRPM*2.0*PI/180.0;
+  spin_slope = omegarads/(STICK_MAX-STICK_MIN);
+  //printf("omegarads = %lf \n",omegarads);
+  
+  //These come from data sheets
+  //at a signal of
+  //double pwm_datapt = STICK_MID;
+  //thrust is
+  //double Tdatapt = 0.735*GEARTH/4.0; //Newtons to kg to lbf
+  
+  //Compute Kt
+  //double dpwm = pwm_datapt - STICK_MIN;
+  //kt = Tdatapt/(dpwm*dpwm);
+  
+  //Angular Velocity computation
+  //double a = (omegaRPMdatapt*2*PI/60.0)/dpwm;
+  
+  //Compute ct and cq
+  ct = thrust_max/(0.5*RHOSLSI*AREA*pow(Rrotor*omegarads,2.0)); //.0335
+  cq = pow(ct,3.0/2.0)/sqrt(2.0); 
+  //printf("CT/CQ = %lf/%lf \n",ct,cq);
+  //PAUSE();
+  
+  //Distance from Cg to rotor
+  rx = (9.0/12.0)/3.28; //meters
+  ry = (9.0/12.0)/3.28; 
+  rz = 0.0;
+}
+
+void forces::compute_thrust_and_torque(int pwm_array[]) {
+  for (int i = 1;i<=4;i++) {
+    double omega = spin_slope*(pwm_array[i-1] - OUTMIN);
+    double thrust = 0.5*RHOSLSI*AREA*pow(omega*Rrotor,2.0)*ct;
+    double torque = 0.5*RHOSLSI*AREA*pow(omega*Rrotor,2.0)*Rrotor*cq;
+    thrust_motors.set(i,1,thrust);
+    torque_motors.set(i,1,torque);
+  }
+  //thrust_motors.disp();
+  //torque_motors.disp();
+}
+
+void forces::ForceMoment(double time,MATLAB state,MATLAB statedot,int pwm_array[],environment env) {
+  //The only thing this function needs to do is populate FB and MB. 
+  //You can do whatever you want in here but you must create those two vectors.
+  FB.mult_eq(0); //Zero these out just to make sure something is in here
+  MB.mult_eq(0);
+
+  //ctlcomms.disp();
+
+  //First we need to convert the microsecond pulse to Newtons
+  compute_thrust_and_torque(pwm_array);
+
+  //Thrust on the body is simply the total thrust
+  double thrust = thrust_motors.sum();
+  //printf("thrust = %lf \n",thrust);
+  FB.set(3,1,-thrust);
+
+  //Torque is a bit more complex
+  //First we add up all the torques in a specific way
+  double yaw_torque = torque_motors.get(1,1) - torque_motors.get(2,1) - torque_motors.get(3,1) + torque_motors.get(4,1);
+  MB.set(3,1,yaw_torque);
+
+  //Then we extract the four forces
+  //From the controller - us signals
+  //ctlcomms.set(1,1,motor_upper_left);
+  //ctlcomms.set(2,1,motor_upper_right);
+  //ctlcomms.set(3,1,motor_lower_left);
+  //ctlcomms.set(4,1,motor_lower_right);
+  //thrust_motors.disp();
+  double motor_upper_left = thrust_motors.get(1,1);
+  double motor_upper_right = thrust_motors.get(2,1);
+  double motor_lower_left = thrust_motors.get(3,1);
+  double motor_lower_right = thrust_motors.get(4,1);
+  
+  //Now we compute torque on roll and pitch
+  double roll_torque = (motor_upper_left+motor_lower_left)*ry - (motor_upper_right+motor_lower_right)*ry;
+  double pitch_torque = (motor_upper_left+motor_upper_right)*rx - (motor_lower_right+motor_lower_left)*rx;
+  MB.set(1,1,roll_torque);
+  MB.set(2,1,pitch_torque);	
+  //MAEROB.disp();
+}
+
+

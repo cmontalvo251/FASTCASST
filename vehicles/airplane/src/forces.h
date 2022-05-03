7ccb0583e7d0c04add6455b7a06dfdc2883341ad
MJCobar <maxwell.cobar@gmail.com>
2022-05-03 14:44:57 -0500

    Updating airplane for aircraft desing

--- vehicles/airplane/src/forces.h
+++ vehicles/airplane/src/forces.h
@@ -18,40 +18,40 @@ functions otherwise the software will completely break.
 #include <RCIO/RCIO.h> //for stick min values
 
 struct Aero_Pack {
-  double CLzero = 0.3441029736813229;
-  double CLalpha = 5.185317204810547;
-  double CLq = 5.9369349224934975;
+  double CLzero = 0.34995;
+  double CLalpha = 4.8872;
+  double CLq = 7.993598;
   double CLdele = 0.41181051485504044;
-  double CDzero = 0.01727851900786801;
-  double CDalpha = 1.0080305157535787;
-  double Cybeta = -0.24095762862664397;
+  double CDzero = 0.015342;
+  double CDalpha = 0.713431;
+  double Cybeta = -0.299134;
   double Cydelr = 0.0688450367504697;
-  double Cyp = -0.027733042384004744;
-  double Cyr = 0.17663419386614151;
-  double CLbeta = -0.01633611041536569;
-  double CLp =  -0.50160400189302223;
-  double CLr = 0.09800094300447591;
+  double Cyp = -0.0423188;
+  double Cyr = 2.04408;
+  double CLbeta = -0.0249279;
+  double CLp =  -0.646828;
+  double CLr = 0.257828;
   double CLdela = 0.28052396489578457;
   double CLdelr = 0.004667460118675911;
-  double Cmzero = -0.07636788929236879;
-  double Cmalpha= -2.1859941783855077;
-  double Cmq = -24.450947269394277;
+  double Cmzero = -0.074721;
+  double Cmalpha= -4.104604;
+  double Cmq = -44.691481;
   double Cmdele = -1.1530526838451003;
-  double Cnp = -0.78826431744966985;
-  double Cnbeta = 0.27846397495417952;
-  double Cnr = -0.33411253656820367;
+  double Cnp = -0.01965;
+  double Cnbeta = 1.022042;
+  double Cnr = -1.167828;
   double Cndela = 0.061169396698422564;
   double Cndelr = -0.014305373822254552; //May need to run a sysID flight on rudder to actually get this one
   double cbar=0.2286;// #Aerodynamic chord length: m
-  double bws=1.4859;  //#Wing span: m
+  double bws=1.372;  //#Wing span: m
   double S = cbar*bws;
-  double mass = 1.2364; //This has got to be Newtons - Maxwell says yes since the a/c is like 2.72 lbf = 2.72*4.44 ~= 12.0
+  double mass = 27.8; //This has got to be Newtons - Maxwell says yes since the a/c is like 2.72 lbf = 2.72*4.44 ~= 12.0
   double Tmax = mass*GEARTH;//these are set in the setup() routine
-  double max_speed = 30; //this is just from anectodal evidence and looking at plots
-  double Rrotor = (9.5/12.0)/(2*3.28); //9.5 inch props to meters
+  double max_speed = 20; //this is just from anectodal evidence and looking at plots
+  double Rrotor = (13./12.0)/(2*3.28); //9.5 inch props to meters
   double AREA = PI*pow(Rrotor,2.0);  //m^2
-  double Voltage = 3.7*4; //4 Cell
-  double KV = 950.0;
+  double Voltage = 3.7*6; //4 Cell
+  double KV = 800.0;
   double omegaRPM = KV*Voltage;
   double omegarads = omegaRPM*2.0*PI/180.0;
   double spin_slope = omegarads/(STICK_MAX-STICK_MIN);

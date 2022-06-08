4a5780aa3729e780480bb1612af8017e69f34e7d
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-06-08 06:43:15 -0500

    Added a serial get number in uart.py and did everything I could with read_serial.py before I have to use hardware to test

--- ground_station/read_serial.py
+++ ground_station/read_serial.py
@@ -2,12 +2,15 @@
 
 import sys
 import time
+import numpy as np
 sys.path.append('../libraries/')
 from UART.uart import UART as U
 ser = U(57600,"/dev/ttyUSB0",period=1.0) #Set the baudrate, port and period in seconds
 ##Make a number array for telemetry
-number_array = np.zeros(6)
+fastkit_packet = np.zeros(6)
 
 while True:
     time.sleep(0.1)
-    print('Checking for new data')
+    print('Checking for new data',time.monotonic())
+    value,position = ser.SerialGetNumber(0)
+    print('Value Received, Position = ',value,position)
--- libraries/UART/uart.py
+++ libraries/UART/uart.py
@@ -6,25 +6,71 @@ class UART():
   def __init__(self,BaudRate=57600,port="/dev/ttyAMA0",period=1.0):
     self.lastTime = 0.0
     self.period = period #in seconds
+    self.MAXLINE = 120
     self.SerialInit(port,BaudRate);
     #//Call this for higher level control
+
+  def bitsToFloat(b):
+    if (b > 2147483647):
+      b = -2147483648 + (b - 2147483647)
+    s = struct.pack('>l', b)
+    return struct.unpack('>f', s)[0]
+
   def SerialInit(self,ComPortName,BaudRate):
     try:
       self.hComm = S.Serial(ComPortName,BaudRate);
     except:
       self.hComm = None
+
   def SerialGetc(self):
-    rxchar = '0' #does nothing right now
+    rxchar = '\0' #does nothing right now
     return rxchar;
+
   def SerialPutc(self,txchar):
     if self.hComm is not None:
       print("Sending ASCII Code = " + str(ord(txchar)))
       self.hComm.write(txchar.encode("ascii"))
+
   def SerialPutString(self,string):
     for s in string:
       self.SerialPutc(s)
-  def SerialGetArray(self):
-    print('Does nothing now')
+
+  def SerialGetNumber(self,echo=1):
+    i = 0;
+    j = 0;
+    inchar = '\0';
+    self.line = ''
+    if (echo):
+      print("Waiting for characters");
+    while inchar != '\r' and i < self.MAXLINE:
+      while inchar == '\0' and j < 1000:
+        inchar = self.SerialGetc();
+        #printf("j = %d i = %d inchar = %c chartoint = %d \n",j,i,inchar,int(inchar));
+        j+=1
+        if (echo):
+          if inchar == '\0':
+            iinchar = 'null'
+          else:
+            iinchar = int(inchar)
+          print('Receing char = ',i,inchar,iinchar)
+          #print("Receiving: i = %d char = %c chartoint = %d \n",i,inchar,int(inchar));
+        self.line += inchar
+        i+=1
+    if (echo):
+      print("Response received",self.line);
+    #Convert to float 
+    if len(self.line) > 11 and self.line[8] == '\r':
+      hexdata = self.line[2:11]
+      print('Hexdata = ',hexdata)
+      integer = int(hexdata,16)
+      value = bitsToFloat(integer)
+      position = 0
+    else:
+      value = 0
+      position = -1
+    return value,position
+
+      
   def SerialSendArray(self,number_array,echo=1):
     #union inparser inputvar; ##Need to look up how to do union inparser
     #in python and then convert the number to 8digit hex

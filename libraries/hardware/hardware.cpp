862fa77a61674360e593f38eb791b587938c20db
MJCobar <maxwell.cobar@gmail.com>
2022-09-23 16:07:09 +0100

    Changing telemetry to send data for GUI ground station

--- libraries/hardware/hardware.cpp
+++ libraries/hardware/hardware.cpp
@@ -165,12 +165,23 @@ void hardware::loop(double currentTime,double elapsedTime,MATLAB control_matrix)
   if (currentTime >= nextTELEMtime) {
     //printf("Sending Telemetry %lf \n",currentTime);
     //For right now let's send RPY and GPS coordinates
-    telemetry_matrix.set(1,1,currentTime);
+
+    //This is the telemetry data needed for ground station command line
+    /*telemetry_matrix.set(1,1,currentTime);
     telemetry_matrix.set(2,1,sense.satellites.latitude);
     telemetry_matrix.set(3,1,sense.satellites.longitude);
     telemetry_matrix.set(4,1,sense.compass);
     telemetry_matrix.set(5,1,sense.orientation.yaw);
-    telemetry_matrix.set(6,1,sense.satellites.heading);
+    telemetry_matrix.set(6,1,sense.satellites.heading);*/
+
+    //This is the telemetry data needed for ground station serial (GUI)
+    telemetry_matrix.set(1,1,currentTime);
+    telemetry_matrix.set(2,1,sense.orientation.roll);
+    telemetry_matrix.set(3,1,sense.orientation.pitch);
+    telemetry_matrix.set(4,1,sense.compass);
+    telemetry_matrix.set(5,1,sense.satellites.latitude);
+    telemetry_matrix.set(6,1,sense.satellites.longitude);
+    
     ser.sendTelemetry(telemetry_matrix,0);
     nextTELEMtime=currentTime+TELEMRATE;
   }

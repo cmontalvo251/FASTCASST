fc2db2a16d05000a459ce1f942081c3f438459c1
Carlos Montalvo on Collin Rpi <cmontalvo251@gmail.com>
2022-10-09 12:48:44 +0000

    Reset basically nan values to -99 in serial.cpp

--- libraries/UART/Serial.cpp
+++ libraries/UART/Serial.cpp
@@ -314,7 +314,11 @@ void Serial::SerialGetArray(float number_array[],int num,int echo) {
     if (echo) {
       printf("Integer Received = %d Float Received = %lf \n",inputvar.inversion,inputvar.floatversion);
     }
-    number_array[d] = inputvar.floatversion;
+    if (inputvar.floatversion > -99999) {
+      number_array[d] = inputvar.floatversion;
+    } else {
+      number_array[d] = -99;
+    }
   }
 }
 

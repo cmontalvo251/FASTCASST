ec94f45e8a77664a1c8125edc68568000c66445d
MJCobar <maxwell.cobar@gmail.com>
2022-10-20 19:48:23 +0100

    made some Rpi serial edits

--- tests/Serial_Test/RPI_Side/ReceiveComputer/read_serial.py
+++ tests/Serial_Test/RPI_Side/ReceiveComputer/read_serial.py
@@ -5,7 +5,7 @@ import time
 import numpy as np
 sys.path.append('../../../../libraries/')
 from UART.uart import UART as U
-ser = U(57600,"/dev/ttyAMA0",period=1.0) #Set the baudrate, port and period in seconds
+ser = U(57600,"/dev/ttyUSB0",period=1.0) #Set the baudrate, port and period in seconds
 
 while True:
     time.sleep(0.1)

33719e090f238228f62a1567e928834ed4f9eba4
Carlos Montalvo T540p <cmontalvo251@gmail.com>
2022-10-27 13:33:12 -0500

    Working on CubeSat. Not Ready to Merge

--- vehicles/satellite/src/controller.cpp
+++ vehicles/satellite/src/controller.cpp
@@ -74,32 +74,36 @@ void controller::loop(double currentTime,int rx_array[],MATLAB sense_matrix) {
     //printf("RUNNING DETUMBLING \n");
     //Extract PQR in (deg/s)
     //sense_matrix.disp();
-    pqr.vecset(1,3,sense_matrix,11);
+    pqr.vecset(1,3,sense_matrix,10);
     //pqr.disp();
     //Convert to (rad/s)
-    pqr.mult_eq(PI/180.0);
+    //pqr.mult_eq(PI/180.0);
     //pqr.disp();
+    desired_moments.vecset(1,3,pqr,1);
+    //desired_moments.disp();
+    desired_moments.mult_eq(pwmC);
+    desired_moments.disp();
     //Extract Magnetomter Readings
-    mxyz.vecset(1,3,sense_matrix,14);
+    //mxyz.vecset(1,3,sense_matrix,13);
     //mxyz.disp();
     //mu_ideal = k*(omega cross B)
-    desired_moments.cross(pqr,mxyz);
-    desired_moments.mult_eq(KMAG);
+    //desired_moments.cross(pqr,mxyz);
+    //desired_moments.mult_eq(KMAG);
     //Convert to currents
-    desired_moments.mult_eq(1.0/(NUMTURNS*AREA));
+    //desired_moments.mult_eq(1.0/(NUMTURNS*AREA));
     //send to ctlcomms (but might not be 3 actuators)
     for (int i = 0;i<NUMSIGNALS;i++) {
       control_matrix.set(i+1,1,desired_moments.get(i+1,1));
     }
     //Saturation on current
-    double sum = control_matrix.abssum();
-    if (sum > MAXCURRENT) {
-      control_matrix.mult_eq(MAXCURRENT/sum);
+    //double sum = control_matrix.abssum();
+    //if (sum > MAXCURRENT) {
+    //control_matrix.mult_eq(MAXCURRENT/sum);
       // for (int i = 0;i<3;i++) {
       //   double val = ctlcomms.get(i+1,1);
       //   ctlcomms.set(i+1,val/sum*maxcurrent);
       // }
-    }
+    //}
   } 
   //control_matrix.disp();
   //printf("SUM = %lf \n",ctlcomms.abssum());

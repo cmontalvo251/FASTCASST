27f968a17b2b5e0a7b226ca6a32c33dc57b63aef
Carlos Montalvo T540p <cmontalvo251@gmail.com>
2022-11-11 09:56:17 -0600

    changed tank rates

--- vehicles/tank/src/controller.cpp
+++ vehicles/tank/src/controller.cpp
@@ -78,7 +78,7 @@ void controller::loop(double currentTime,int rx_array[],MATLAB sense_matrix) {
     //printf("yaw = %lf \n",yaw);
     throttle_command = STICK_MID + 0.5*(STICK_MAX-STICK_MID);
     double yaw_command = 0;
-    aileron_command = STICK_MID - 5*(yaw-yaw_command);
+    aileron_command = STICK_MID - 50*(yaw-yaw_command);
   } else {
     throttle_command = throttle;
     aileron_command = aileron;

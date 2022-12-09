a169c70d393a3bd23e50e90a955f937f85f26476
Carlos Montalvo T540p <cmontalvo251@gmail.com>
2022-12-09 11:43:52 -0600

    Edited the plotting field test data code

--- plotdata_fieldtest.py
+++ plotdata_fieldtest.py
@@ -16,7 +16,7 @@ except:
     sys.exit()
 
 ##TRUNCATION START AND END TIME of flight (Set to negative to turn off)
-tstart = 80
+tstart = 120
 tend = 200
 
 ##Create PDF Handle
@@ -138,12 +138,65 @@ kde = 0.5;
 OUTMID = 1500;
 OUTMAX = 2016
 OUTMIN = 992
-roll_command = roll - (pwm_aileron - kda*roll_rate - OUTMID)/kpa;
+roll_command = roll - (-(pwm_aileron - OUTMID)-kda*roll_rate)/kpa;
 #aileron = kpa*(roll-phiC_pwm) + kda*(roll_rate)
 phi_command = (phiC_pwm-OUTMID)*50.0/((OUTMAX-OUTMIN)/2.0)
 pitch_command = pitch - (pwm_elevator - kde*pitch_rate - OUTMID)/kpe;
 #elevator = kpe*(pitch-thetaC_pwm) + kde*(pitch_rate)
 theta_command = -(thetaC_pwm-OUTMID)*30.0/((OUTMAX-OUTMIN)/2.0)
+
+##Plot
+fig = plt.figure()
+plti = fig.add_subplot(1,1,1)
+plti.plot(sense_flighttime,roll,label="Roll Angle")
+#plti.plot(sense_flighttime,roll_command,label="Calculated Roll Command")
+plti.plot(sense_flighttime,phi_command,label="RX Roll Command")
+plti.set_xlabel('Time (sec)')
+plti.set_ylabel('Roll Angle (deg)')
+plti.grid()
+plti.legend() 
+plti.get_yaxis().get_major_formatter().set_useOffset(False)
+plti.get_xaxis().get_major_formatter().set_useOffset(False)
+plt.gcf().subplots_adjust(left=0.18)
+pp.savefig()
+
+fig = plt.figure()
+plti = fig.add_subplot(1,1,1)
+plti.plot(sense_flighttime,abs(roll-phi_command))
+plti.set_xlabel('Time (sec)')
+plti.set_ylabel('Roll Angle Error (deg)')
+plti.grid()
+plti.get_yaxis().get_major_formatter().set_useOffset(False)
+plti.get_xaxis().get_major_formatter().set_useOffset(False)
+plt.gcf().subplots_adjust(left=0.18)
+pp.savefig()
+
+##Plot
+fig = plt.figure()
+plti = fig.add_subplot(1,1,1)
+plti.plot(sense_flighttime,pitch,label="Pitch Angle")
+plti.plot(sense_flighttime,pitch_command,label="Calculated Pitch Command")
+plti.plot(sense_flighttime,theta_command,label="RX Pitch Command")
+plti.set_xlabel('Time (sec)')
+plti.set_ylabel('Pitch Angle (deg)')
+plti.grid()
+plti.legend() 
+plti.get_yaxis().get_major_formatter().set_useOffset(False)
+plti.get_xaxis().get_major_formatter().set_useOffset(False)
+plt.gcf().subplots_adjust(left=0.18)
+pp.savefig()
+
+fig = plt.figure()
+plti = fig.add_subplot(1,1,1)
+plti.plot(sense_flighttime,abs(pitch-theta_command))
+plti.set_xlabel('Time (sec)')
+plti.set_ylabel('Pitch Angle Error (deg)')
+plti.grid()
+plti.get_yaxis().get_major_formatter().set_useOffset(False)
+plti.get_xaxis().get_major_formatter().set_useOffset(False)
+plt.gcf().subplots_adjust(left=0.18)
+pp.savefig()
+
 ##Plot
 fig = plt.figure()
 plti = fig.add_subplot(1, 1, 1)

fec5ec6c4eeef802169f6e735eedde03b87dbab5
Carlos Montalvo <cmontalvo@southalabama.edu>
2024-01-12 13:09:48 -0600

    Added TopGun files

---
+++ Archive/FASTWing/TopGun/archive/TopGun_Montalvo.cpp
@@ -0,0 +1,469 @@
+#include <cstdio>
+#include <Navio2/Led_Navio2.h>
+#include <Navio2/RCInput_Navio2.h>
+#include <Common/Util.h>
+#include "Navio2/PWM.h"
+#include "Navio2/RCOutput_Navio2.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <memory>
+#include <stdint.h>
+#include <sys/time.h>
+#include <Common/MPU9250.h>
+
+#define G_SI 9.80665
+#define SERVO_MAX 1900 /*mS*/
+#define SERVO_MIN 1100 /*ms*/
+#define THROTTLE_MIN 994
+#define THROTTLE_MAX 2100
+#define IDLE 1200 /*mS*/
+#define READ_FAILED -1
+
+//#define DEBUGPRINTS
+//#define USEMAD //use this for the madgwick filter
+#define USEAHRS //use this for the AHRS filter
+//#define USEKYRELLPID
+#define USEMONTALVOPID
+//#define USERECONFIG
+#define LOGDATA
+
+using namespace std;
+using namespace Navio;
+
+//If we're using the reconfigurable controller we need a few things
+#define PI   3.14159
+#define RAD2DEG 180.0/PI
+#define DEG2RAD PI/180.0
+//Operands
+#define CONSTRAIN(in,min,max) (in > max ? max : (in < min ? min : in))
+#define wrap_Pi(x) (x < -Pi ? x+Pi2 : (x > Pi ? x - Pi2: x))
+#define wrap_180(x) (x < -180 ? x+360 : (x > 180 ? x - 360: x))
+
+#ifdef LOGDATA
+#include "links/Datalogger.h"
+#include "links/MATLAB.h"
+MATLAB OUTPUT;
+int FILEOPEN = 0;
+Datalogger logger;
+#endif
+
+//Objects
+//Always need a sensor
+InertialSensor *imu;
+
+//Need an Led
+Led_Navio2 *led;
+
+//Need a PWM object
+RCOutput_Navio2 rcout;
+
+//and a rx object - I edited the constructor class to run the initialize routine
+//check RCInput_Navio2.cpp
+RCInput_Navio2 rcin;
+
+//The type of filter is up for debate
+#ifdef USEMAD
+#include "Navio2/AHRS2.hpp"
+AHRS ahrs_mad;   // Madgwick AHRS --Let's use only the MADWICK FILTER
+#endif
+#ifdef USEAHRS
+#include "Navio2/AHRS.hpp"
+AHRS ahrs;
+#endif
+
+// Sensor data
+float ax=0, ay=0, az=0;
+float gx=0, gy=0, gz=0;
+float mx=0, my=0, mz=0;
+float qw, qx, qy, qz;
+float negativeone = -1.0;
+//Date for PID
+float roll = 0 , pitch = 0 , yaw = 0;
+float gx_filtered=0, gy_filtered=0, gz_filtered=0;
+
+// Timing data
+float offset[3];
+struct timeval tv;
+float dt, maxdt;
+float mindt = 0.01;
+unsigned long previoustime, currenttime;
+float dtsumm = 0;
+int isFirst = 1;
+static unsigned long lastReceiverTime = 0;
+
+//Receiver Signals
+int arm_switch;
+int throttle;
+int rollrc;
+int pitchrc;
+int yawrc;
+int pusherpwm;
+int configrc;
+//Commands created by receiver
+float roll_command=0,pitch_command=0,yaw_rx=0,thrust_desired=0;
+float mass=1.2364;
+
+//PID Loop gains and other stuff for control
+float kproll,kdroll,d_roll=0,kppitch,kdpitch,d_pitch=0,d_yaw=0,kipitch,kiroll;
+float yawLock = 0;
+float perrorIntegral=0,qerrorIntegral=0;
+float pitcherrorIntegral=0,rollerrorIntegral=0;
+double MOTORSOUT[4];
+double MOTORSOUT_[4];
+double inSIG_[4];
+double inSIG[4];
+
+#include "links/Aerodynamics.h"
+Aerodynamics aero;
+
+void ControlLoop(double elapsedTime) {
+
+  //Call the PIDCONTROL ROUTINE HERE -- Need to send it sensor information
+  //void Aerodynamics::PIDCONTROL(float u,float z,float zdot,float theta,float q,float phi,float p,float r) {
+  float u,z,zdot,theta,q,phi,p,r;
+  aero.PIDCONTROL(u,z,zdot,theta,q,phi,p,r);
+  
+  //Added this filter here. This is a standard first order filter using a tustin transformation
+  //the a here is a pole for the first order transfer function such that G = a/(s+a) thus if you
+  //make a bigger the system will filter less but repond faster.
+  //In plot_flight_data.py I have a = 3.0 which is a ton of filtering and there's no way that would
+  //work in flight. Any filter is better than none though. 
+  //So far I have tried 16 and the system was unstable.
+  //Trying 25 yielded undesiredable results in the lab but outside on 4/1/2019 and it took off with horrible oscillations. I would say it was marginally stable.
+  //Suggest increasing to 35.0
+  //Setting the value to 35.0 made the system respond very well. This is still untested with shutting off motors but other than a few oscillations the system
+  //did seem a heck of a lot smoother than before. Still not 100% positive how the data looks thought but I suggest plotting output28.txt
+  double a = 35.0;
+  double tau = 1.0/a;
+  double alfa = 2*tau/elapsedTime;
+  for (int i = 0;i<=4;i++) {
+    inSIG_[i] = inSIG[i]; //Reset old signal
+    inSIG[i] = aero.controls.get(i+1,1);
+    MOTORSOUT_[i] = MOTORSOUT[i];
+    MOTORSOUT[i] = (inSIG[i]+inSIG_[i]-MOTORSOUT_[i]*(1-alfa))/(alfa+1);
+  }
+
+  //Saturation Blocks -- Needs to be changed for servos and throttle
+  for (int i = 0;i<=8;i++) {
+    MOTORSOUT[i] = CONSTRAIN(MOTORSOUT[i],IDLE,SERVO_MAX);
+  }
+}
+
+int imuSetup()
+{
+  printf("Selected: MPU9250\n");
+  imu = new MPU9250();
+
+  ///Initialize the Kalman Filter shit. -- How much money do you think this guy makes???
+  //imu = create_inertial_sensor(sensor_name);
+  //Actually this is the IMU. ended up overhauling the code after we called Shawn from Kansas City.
+  //We do this regardless of the filter type that we use
+
+  ///This sends out a probe. Quick recon
+  if (!imu->probe()) {
+    printf("Sensor not enable\n");
+    return 0;
+  }
+  
+  //Setup for MADGWICK FILTER
+  imu->initialize();
+  printf("Beginning Gyro calibration...\n");
+  offset[0] = 0;
+  offset[1] = 0;
+  offset[2] = 0;
+  for(int i = 0; i<100; i++)
+    {
+      imu->update();
+      imu->read_gyroscope(&gx, &gy, &gz);
+      gx *= 180 / PI;
+      gy *= 180 / PI;
+      gz *= 180 / PI;
+      offset[0] += gx*0.0175;
+      offset[1] += gy*0.0175;
+      offset[2] += gz*0.0175;
+      usleep(10000);
+    }
+  offset[0]/=100.0;
+  offset[1]/=100.0;
+  offset[2]/=100.0;
+  
+  printf("Offsets are: %f %f %f\n", offset[0], offset[1], offset[2]);
+#ifdef USEMAD
+  ahrs_mad.setGyroOffset(offset[0], offset[1], offset[2]);
+#endif
+
+  //Setup for AHRS Filter
+#ifdef USEAHRS
+  ahrs.setGyroOffset(offset[0],offset[1],offset[2]);
+#endif
+  
+  return 1;
+  
+}
+
+static unsigned long getCurrentTime() {
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  static unsigned long currentTime;
+  currentTime = 1000000 * tv.tv_sec + tv.tv_usec;
+  return currentTime;
+}
+
+//============================== Main IMU loop ====================================
+
+void imuLoop(float elapsedTime)
+{
+  imu->update();
+  imu->read_accelerometer(&ax, &ay, &az);
+  imu->read_gyroscope(&gx, &gy, &gz);
+  
+#ifdef USEMAD
+  imu->read_magnetometer(&mx, &my, &mz);
+  ax /= G_SI;
+  ay /= G_SI;
+  az /= G_SI;
+  gx *= 180 / PI;
+  gy *= 180 / PI;
+  gz *= 180 / PI;
+  ahrs_mad.updateMadgwick(ax, ay, az, gy*0.0175, gx*0.0175, gz*0.0175, my, mx, -mz,elapsedTime);
+  ahrs_mad.getEuler(&pitch, &roll, &yaw);
+#endif
+
+#ifdef USEAHRS
+  //ahrs.update(ax,ay,az,gx,gy,gz,mx,my,mz,elapsedTime);
+  ahrs.updateNOMAG(ax,ay,az,gx,gy,gz,elapsedTime);
+  ahrs.getEuler(&pitch,&roll,&yaw);
+#endif
+
+  //Run Complimentary Filter to get angular rate filtered
+  //float time_constant = 0.5; // Number must be between 0 and 1.
+  //gx_filtered += elapsedTime*(time_constant*gx + (1.0-time_constant)*gx_filtered);
+  //gy_filtered += elapsedTime*(time_constant*gy + (1.0-time_constant)*gy_filtered);
+  //gz_filtered += elapsedTime*(time_constant*gz + (1.0-time_constant)*gz_filtered);
+  
+  //Ok fuck complimentary filters. Running a First order Filter using Euler's method
+  //float settling_time = 0.01;
+  //float time_constant = 4.0/settling_time;
+  //printf("Elapsed Time = %lf \n",elapsedTime);
+  //printf("Time Constant = %lf \n",time_constant);
+  //For Euler's method to work elapsedTime*time_constant < 1
+  //When we were running this elapsedTime = 0.018 so let's let time_consant = 10
+  // float time_constant = 10;
+  // gx_filtered += elapsedTime*time_constant*(gx-gx_filtered);
+  // gy_filtered += elapsedTime*time_constant*(gy-gy_filtered);
+  // gz_filtered += elapsedTime*time_constant*(gz-gz_filtered);
+  float s = 0.35; //Changed filter from 0.2 to 0.1 on 2/28/2019. Changed to 0.2 again on 3/11/2019 to try and remove some noise. Filtered changed from 0.35 to 0.4 on 3/11/19. Did not like it so changed it back.
+  gx_filtered = gx_filtered*s + (1-s)*gy*RAD2DEG;
+  gy_filtered = gy_filtered*s + (1-s)*gx*RAD2DEG;
+  gz_filtered = gz_filtered*s + (1-s)*(-gz)*RAD2DEG;
+}
+
+
+//=====================================Main Loop Start====================================
+int main(int argc, char *argv[])
+{
+
+  #ifdef LOGDATA
+  //Create OUTPUT Matrix
+  //time, throttleRC, rollRC, pitchRC, yawRC, roll,pitch,yaw,gx,gy,gz,9 motor signals
+  OUTPUT.zeros(20,1,"OUTPUT MATRIX");
+  #endif
+  
+  //Set some LED status
+  led = new Led_Navio2();
+  if (!led->initialize()) {
+    return EXIT_FAILURE;
+  }
+  //Set Color to Yellow
+  led->setColor(Colors::Yellow);
+  
+  //Check and see if you are a root user
+  if (getuid()) {
+    fprintf(stderr, "Not root. Please launch like this: sudo %s\n", argv[0]);
+  }
+  
+  //Check for ardupilot running
+  if (check_apm()) {
+    return 1;
+  }
+
+  //Setup the receiver
+  //double RXCHANNELS[NUM_CHANNELS],RXVALS[NUM_CHANNELS];
+  rcin.NUMRX = 5;
+  rcin.RXCHANNELS[0] = 0; //roll is channel 0
+  rcin.RXCHANNELS[1] = 1; //pitch is channel 1
+  rcin.RXCHANNELS[2] = 2; //throttle in channel 2
+  rcin.RXCHANNELS[3] = 3; //yaw is channel 3
+  rcin.RXCHANNELS[4] = 6; //arm_switch is channel 6
+  rcin.RXCHANNELS[5] = 4; //configuration switch
+  rcout.num_motors = 4;
+  rcout.PWMOUTPUTS[0] = 0;
+  rcout.PWMOUTPUTS[1] = 1;
+  rcout.PWMOUTPUTS[2] = 2;
+  rcout.PWMOUTPUTS[3] = 3;
+  rcout.PWMOUTPUTS[4] = 4;
+  
+  int out_res = rcout.rcoutSetup();
+  if (out_res) {
+    return EXIT_FAILURE;
+  }
+  sleep(1);
+  //Setup the IMU.
+  int imu_res = imuSetup();
+  if (!imu_res) {
+    return EXIT_FAILURE;
+  }
+
+  //===================================
+  ////^^^^Everything above here is like a setup routine
+
+  static unsigned long currentTime = getCurrentTime();
+  static unsigned long previousTime = 0;
+  float elapsedTime = 0;
+  ///This is the equivalent of a void loop() routine on Arduino
+  static unsigned long beginTime = getCurrentTime();
+  lastReceiverTime = beginTime;
+  float printTime = beginTime;
+  float dataTime = beginTime;
+  float timeSinceStart = 0;
+
+  printf("Time For take off \n"); //Oh shit.
+  sleep(1);
+  printf("This shits about to get #nasty #nohomo #shemakemedoit \n");
+
+  printf("But first....wait 10 seconds for the red light \n");
+
+  //MAIN LOOP
+  while (true)
+    {
+      //Get Current time
+      previousTime = currentTime;
+      currentTime = getCurrentTime();
+      elapsedTime = (currentTime - previousTime) / 1000000.0; 
+      timeSinceStart = (currentTime - beginTime) / 1000000.0;
+
+      //Poll Receiver at 10 Hz
+      if ((currentTime - lastReceiverTime)/1000000.0 > 0.1) {
+	//Save Current Time
+	lastReceiverTime = currentTime;
+	//Poll Receiver
+	//printf("Polling Receiver \n");
+      	rcin.pollReceiver();
+	//printf("Receiver Polled \n");
+	rollrc = rcin.RXVALS[0];
+	pitchrc = rcin.RXVALS[1];
+	throttle = rcin.RXVALS[2];
+	yawrc = rcin.RXVALS[3];
+	arm_switch = rcin.RXVALS[4];
+	configrc = rcin.RXVALS[5];
+	//printf("Values extracted \n");
+	//================make commands===============
+	//i need to take the pwm signals and make them angle commands
+	//These commands only need to be generated when you poll the receiver
+	roll_command = (50.0/500.0)*(float(rollrc)-1500.0);
+	pitch_command = (50.0/500.0)*(float(pitchrc)-1500.0);
+	yaw_rx = (135.0/500.0)*(float(yawrc)-1500.0); //this is really a yawRate command
+	thrust_desired = ((1.0)/((double)THROTTLE_MAX-(double)THROTTLE_MIN))*(throttle-THROTTLE_MIN);
+      }
+
+      //I'm pretty sure this is what gives us the ptp values
+      imuLoop(elapsedTime); //why does it take so long to get through this loop?
+
+      //================write outputs===============
+      if(arm_switch > 1300) {
+	led->setColor(Colors::Green);
+	//================Controller==============
+	//This runs every timestep since imuLoop is running everytimestep
+	ControlLoop(elapsedTime);
+	/////////////////////////////////////////
+	#ifdef LOGDATA
+	if (FILEOPEN == 0) {
+	  //The file is not open so open it
+	  logger.findfile("Data_Files/output");
+	  logger.open();
+	  FILEOPEN = 1; //Set this flag to 1 to signify that the file is open
+	} //else {
+	  //The file is already open so don't do anything?
+
+       } else {
+	//Check and see if we need to close the file
+	if (FILEOPEN == 1) {
+	  FILEOPEN = 0;
+	  logger.close();
+	}
+	#endif
+	
+	//Otherwise just send servo_min to all motors
+       	if (timeSinceStart > 10) {
+       	  led->setColor(Colors::Red);
+       	}
+	//Reset integral error
+	pitcherrorIntegral = 0;
+	rollerrorIntegral = 0;
+	perrorIntegral = 0;
+	qerrorIntegral = 0;
+	for (int i = 0;i<=8;i++) {
+	  MOTORSOUT[i] = SERVO_MIN;
+	  MOTORSOUT_[i] = SERVO_MIN;
+	  inSIG_[i] = SERVO_MIN;
+	  inSIG[i] = SERVO_MIN;
+	}
+      } //end of arm switch
+      //Send signals to motors
+      //carlos came up with the signal naming convention, i think it is bullshit.
+      //Collin is right. I'm sorry. It's been changed
+      for (int i = 0;i<=5;i++) {
+	rcout.set_duty_cycle(i,MOTORSOUT[i]);
+      }
+
+      #ifdef LOGDATA
+      if ((((currentTime - dataTime)/1000000.0) > 0.02) && (FILEOPEN == 1)) {
+	dataTime = currentTime;
+	OUTPUT.set(1,1,currentTime);
+	OUTPUT.set(2,1,throttle);
+	OUTPUT.set(3,1,rollrc);
+	OUTPUT.set(4,1,pitchrc);
+	OUTPUT.set(5,1,yawrc);
+	OUTPUT.set(6,1,roll);
+	OUTPUT.set(7,1,pitch);
+	OUTPUT.set(8,1,yaw);
+	OUTPUT.set(9,1,gx_filtered);
+	OUTPUT.set(10,1,gy_filtered);
+	OUTPUT.set(11,1,gz_filtered);
+	for (int i = 12;i<(12+5);i++) {
+	  OUTPUT.set(i,1,MOTORSOUT[i-12]);
+	}
+	logger.println(OUTPUT);
+	//printf("Data logged \n");
+	//log.flush(); No longer need to flush - arm_switch accomplishes this
+      }
+      #endif
+
+      #ifdef DEBUGPRINTS
+      if ((currentTime - printTime) / 1000000.0 > 0.05) { //print at 20 Hz
+	printTime = currentTime;
+	//Time
+	printf("%lf %lf ",printTime/1000000.0,elapsedTime);
+	//PTP
+	//printf("%+05.2f %+05.2f %+05.2f ",roll,pitch,yaw);
+	//PQR
+	//printf("%+05.2f %+05.2f %+05.2f ",gx,gy,gz);
+	//PQR_filtered
+	//printf("%+05.2f %+05.2f %+05.2f ",gx_filtered,gy_filtered,gz_filtered);
+	//Rec Signals
+	printf("%d %d %d %d %d %d",throttle,rollrc,pitchrc,yawrc,configrc,arm_switch);
+	//Rec Commands
+	printf("%lf ",thrust_desired);
+	//PWM outputs
+	for (int i = 0;i<=5;i++) {
+	  printf("%lf ",MOTORSOUT[i]);
+	}
+	printf("\n");
+      }
+      #endif
+
+    } //end of void loop
+
+  return 0;
+}

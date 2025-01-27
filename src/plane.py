d7fc1decff50e7ae952e726e1266e2c81d3b1148
Carlos Montalvo RPi4 #2 <cmontalvo251@gmail.com>
2025-01-27 17:31:06 +0000

    Added plane.py to src

---
+++ src/plane.py
@@ -0,0 +1,165 @@
+
+#############################################
+#
+#  FASTPy - Facility for Aerial Systems and Technology
+#  Kit in Python
+#
+#  Standard 4 Channel Airplane (Derived from Car.py)
+#  Created: Spring 2025
+#  Primary Author: Kate Doiron
+#
+################################################
+
+
+#Add in all needed libraries and modules
+import sys, time
+
+sys.path.append('/home/pi/FASTCASST/libraries/Util')
+import util
+
+sys.path.append('/home/pi/FASTCASST/libraries/GPS')
+import gps
+
+sys.path.append('/home/pi/FASTCASST/libraries/MPU9250')
+import mpu9250
+
+sys.path.append('/home/pi/FASTCASST/libraries/Datalogger')
+import datalogger
+
+sys.path.append('/home/pi/FASTCASST/libraries/LED')
+import leds
+
+sys.path.append('/home/pi/FASTCASST/libraries/RCIO/Python')
+import rcinput
+import pwm
+
+import numpy as np
+#Create a time for elapsed time
+print('Setting up Time')
+StartTime = time.time()
+GPSTime = time.time()
+
+#Make sure Ardupilot is off
+print('Checking to make sure APM is off')
+util.check_apm()
+
+#Setup datalogger
+logger = datalogger.Datalogger()
+print(sys.argv)
+logger.findfile(sys.argv[1])
+logger.open()
+#create an array for data
+#arm,throttlerc,yawrc,lat,lon,alt,velocity,roll,pitch,heading
+outdata = np.zeros(10)
+
+#Setup GPS
+print('Initializing GPS...')
+gps_llh = gps.GPS()
+gps_llh.initialize()
+
+#Setup IMU
+print('Initializing IMU...')
+imu = mpu9250.MPU9250()
+imu.initialize()
+
+#Setup RCIO
+print('Initializing RCInput...')
+rcin = rcinput.RCInput()
+i = 0
+num_channels = 9
+
+#Setup LED
+print('Setting up LEDs.....')
+led = leds.Led()
+led.setColor('Yellow')
+print('LED is yellow now')
+
+#Setup Servos
+SERVO_MIN = 0.995 #ms
+SERVO_MID = 1.504 #ms
+SERVO_MAX = 2.010 #ms
+PWM_OUTPUT = [0,1] #Servo Rail Spots
+print('PWM Channels: ',PWM_OUTPUT)
+
+#Throttle - PWM Channel 1
+print('Initializing PMW channels')
+pwm1 = pwm.PWM(PWM_OUTPUT[0])
+pwm1.initialize()
+pwm1.set_period(50)
+pwm1.enable()
+#Steering - PWM Channel 2
+pwm2 = pwm.PWM(PWM_OUTPUT[1])
+pwm2.initialize()
+pwm2.set_period(50)
+pwm2.enable()
+
+#Short break to build suspense
+print('Sleep for 1 second')
+time.sleep(1)
+
+#This runs on repeat until code is killed
+while (True):
+    RunTime = time.time() - StartTime
+    elapsedTime = RunTime - GPSTime
+    #print(elapsedTime)
+    
+    #Read in receiver commands
+    period = []
+    for i in range(num_channels):
+        value = rcin.read(i)
+        period.append(value)
+    #print period
+
+    #Turn receiver commands to floats
+    throttlerc = float(period[0])/1000.
+    rollrc = float(period[1])/1000.
+    pitchrc = float(period[2])/1000.
+    yawrc = float(period[3])/1000.
+    armswitch = float(period[4])/1000.
+    #print(throttlerc,rollrc,pitchrc,yawrc,armswitch)
+
+    #Get GPS update
+    if(elapsedTime > 1.0):
+        GPSTime = time.time()
+        gps_llh.update()
+        #print(gps_llh.longitude,gps_llh.latitude,gps_llh.altitude)
+
+    #Set arm switch up for safety reasons
+    if(armswitch < 1.495):
+        led.setColor('Red')
+        pwm1.set_duty_cycle(SERVO_MIN)
+        pwm2.set_duty_cycle(SERVO_MID)
+        #print('Disarmed for safety')
+    elif(1.495 < armswitch < 1.995):
+        led.setColor('Green')
+        #Add Saturation Block to ensure servo safety
+        if(throttlerc < SERVO_MIN):
+            throttlerc = SERVO_MIN
+        if(throttlerc > SERVO_MAX):
+            throttlerc = SERVO_MAX
+        #Send throttlerc to servo
+        pwm1.set_duty_cycle(throttlerc)
+        #Add Saturation Block to ensure servo safety
+        if(yawrc < SERVO_MIN):
+            yawrc = SERVO_MIN
+        if(yawrc > SERVO_MAX):
+            yawrc = SERVO_MAX
+        #Send yawrc to servo
+        pwm2.set_duty_cycle(yawrc)
+        #print('Open Loop Control')
+    elif(armswitch > 1.995):
+        led.setColor('Blue')
+        pwm1.set_duty_cycle(SERVO_MIN)
+        pwm2.set_duty_cycle(SERVO_MID)
+        #print('Autonomous Control')
+    #print(armswitch,throttlerc,yawrc)
+
+    #Print to Home
+    print(np.round(RunTime,2),throttlerc,rollrc)
+
+    #Log data
+    outdata[0] = armswitch
+    outdata[1] = throttlerc
+    outdata[2] = yawrc
+    #outdata[]
+    logger.println(outdata)
